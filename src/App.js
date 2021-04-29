90a1cc5c405f3295a48514e362447e11fd314872
alexa <alexandercpride@gmail.com>
2021-04-29 12:45:48 +1200

    Restructured to add routing to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+import Home from "./pages/home.js";
+import Main from "./pages/main.js";
+import Redirect from "./pages/redirect.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Switch>
+
+        <Route path={"/redirect"}>
+          <Redirect/>
+        </Route>
+
+        <Route path={"/main"}>
+          <Main/>
+        </Route>
+
+        <Route exact path={"/"}>
+          <Home/>
+        </Route>
+
+      </Switch>
+
+    </Router>
   );
 }
 
