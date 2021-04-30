e06feda7b9bd1ea3dd9ad2bf8247d31f05f1a190
kian <foulkekian@ecs.vuw.ac.nz>
2021-04-30 15:12:52 +1200

    basic code for grouping emails

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,32 +1,112 @@
-import React,{useEffect,useState} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useLocation } from "react-router-dom";
 
 function Main(props) {
-    const location = useLocation();
-    const {userId,accessToken} = location.state;
-    const [emails,setEmails] = useState([])
+  const location = useLocation();
+  const { userId, accessToken } = location.state;
+  const [threads, setThread] = useState([]);
+  const [emails, setEmails] = useState([]);
+  const [group, setGroup] = useState(new Map());
+  const groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
 
+  useEffect(() => {
+    const instance = axios.create({
+      baseURL: `https://gmail.googleapis.com/`,
+      headers: { authorization: "Bearer " + accessToken },
+    });
+    instance
+      .get(`gmail/v1/users/${userId}/threads`)
+      .then((res) => res.data)
+      .then((data) => {
+        console.log("we are here: ", data);
+        setThread(data.threads);
+      });
+
+    instance
+      .get(`gmail/v1/users/${userId}/messages`)
+      .then((res) => res.data)
+      .then((data) => {
+        console.log("we are here with messagesssssss: ", data);
+        data.messages.forEach((message) => {
+          instance
+            .get(`gmail/v1/users/${userId}/messages/${message.id}`)
+            .then((res) => res.data)
+            .then((localData) => {
+              console.log(localData);
+              message.message = localData;
+              localData.payload.headers.forEach((obj) => {
+                if (obj.name === "From") {
+                  const arr = group.get(obj.value);
+                  if (arr) {
+                    arr.push(message);
+                  } else {
+                    group.set(obj.value, [message]);
+                  }
+                  setGroup(new Map(group));
+                }
+              });
+              setEmails([...data.messages]);
+            });
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    console.log("group:::: ", group);
+    console.log("group entires: ", group.entries());
     
-    useEffect(()=>{
-        const instance = axios.create({   baseURL: `https://gmail.googleapis.com/`,     headers :{ 'authorization': 'Bearer ' + accessToken } })
-        instance.get(`gmail/v1/users/${userId}/threads`).then(res=>res.data).then(data=>{console.log("we are here: ",data);setEmails(data.threads)});
+  }, [group]);
 
-    },[])
-    return (
-        <div>
-            <h1>Emails</h1>
-            <ul>
-            {emails.map(element => {
-                console.log("element:",element);
-                return <li>{element.snippet.length>100 ? element.snippet.substring(0,100)+"..." :element.snippet }</li>
-            })}
-            </ul>
-        </div>
-    )
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ flexGrow: 1 }}>
+        <h1>Emails</h1>
+        <ul>
+          {emails.map((element) => {
+            return (
+              <li key={element.id}>
+                {element.message
+                  ? element.message.snippet.length > 100
+                    ? element.message.snippet.substring(0, 100) + "..."
+                    : element.message.snippet
+                  : "Loading"}
+              </li>
+            );
+            // return <p>hi</p>; //<li>{element.snippet.length>100 ? element.snippet.substring(0,100)+"..." :element.snippet }</li>
+          })}
+        </ul>
+      </div>
+      <div style={{ flexGrow: 1 }}>
+        <h1>Threads</h1>
+        <ul>
+          {threads.map((element) => {
+            return (
+              <li key={element.id}>
+                {element.snippet.length > 100
+                  ? element.snippet.substring(0, 100) + "..."
+                  : element.snippet}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div>
+        {Array.from(group.entries()).map((value) => {
+            const from = value[0];
+            const messages = value[1];
+            
+          return <div><h1>{from}</h1><ul>{messages.map((messageInfo)=>{return <li>{messageInfo.id}</li>})}</ul></div>
+        })}
+      </div>
+    </div>
+  );
 }
 
-
-
-export default Main
-
+export default Main;
